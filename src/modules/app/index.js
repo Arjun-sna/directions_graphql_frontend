8b290d2667b856755d36012557288cd2d8d071a1
Arjun sn <snarjun92@gmail.com>
2019-07-26 17:23:32 +0530

    setup apollo client in app

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import Layout from '~/components/layout';
 import Routes from './routes';
 import AppContextProvider from './contextProvider';
+
+const apolloClient = new ApolloClient({
+  uri: 'https://directions-graphql.herokuapp.com/graphql'
+});
 
 export default () => {
   
   return (
     <AppContextProvider>
-      <Router>
-        <Layout>
-          <Routes />
-        </Layout>
-      </Router>
+      <ApolloProvider client={apolloClient}>
+        <Router>
+          <Layout>
+            <Routes />
+          </Layout>
+        </Router>
+      </ApolloProvider>
     </AppContextProvider>
   );
 };
