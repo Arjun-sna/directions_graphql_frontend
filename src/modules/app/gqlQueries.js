e39b585ae1bc37ed2576fdbd2f62494dd00a55bc
Arjun sn <snarjun92@gmail.com>
2019-07-29 11:26:36 +0530

    add direction query

--- src/modules/app/gqlQueries.js
+++ src/modules/app/gqlQueries.js
@@ -15,3 +15,108 @@ export const SIGN_UP = gql`
     }
   }
 `
+
+export const GET_DIRECTION = gql`
+  fragment TransitPointDetailsParts on TransitPointDetails {
+    formatedTime
+    address
+    timeZone
+    timeValue
+    location {
+      latitude
+      longitude
+    }
+  }
+  fragment OtherPointDetailsParts on OtherPointDetails {
+    address
+    location {
+      latitude
+      longitude
+    }
+  }
+  fragment TripDataPart on TripData {
+    arrival {
+      ... on TransitPointDetails {
+        ...TransitPointDetailsParts
+      }
+      ... on OtherPointDetails {
+        ...OtherPointDetailsParts
+      }
+    }
+    departure {
+      ... on TransitPointDetails {
+        ...TransitPointDetailsParts
+      }
+      ... on OtherPointDetails {
+        ...OtherPointDetailsParts
+      }
+    }
+    tripDuration {
+      ...TypedDataParts
+    }
+    tripDistance {
+      ...TypedDataParts
+    }
+  }
+  fragment TypedDataParts on TypedData {
+    formattedValue
+    rawValue
+  }
+  fragment CoordsPart on Coords {
+    latitude
+    longitude
+  }
+  query($coordinates: PlaceCoordinatesInput!, $travelMode: AllowTravelModes!) {
+    direction(coordinates: $coordinates, travelMode: $travelMode) {
+      fare {
+        formattedFare
+        currency
+        fareValue
+      }
+      tripData {
+        ...TripDataPart
+      }
+      steps {
+        stepTravelMode
+        stepDistance {
+          ...TypedDataParts
+        }
+        stepDuration {
+          ...TypedDataParts
+        }
+        startLocation {
+          ...CoordsPart
+        }
+        endLocation {
+          ...CoordsPart
+        }
+        polyline
+        stepInstruction
+        ... on WalkStep {
+          walkSteps {
+            stepTravelMode
+            stepInstruction
+          }
+        }
+        ... on TransitStep {
+          arrival {
+            ...TransitPointDetailsParts
+          }
+          departure {
+            ...TransitPointDetailsParts
+          }
+          transitData {
+            tripName
+            tripShortName
+            url
+            headSign
+            stopsCount
+            vehicleIcon
+            vehicleName
+            vehicleType
+          }
+        }
+      }
+    }
+  }
+`
