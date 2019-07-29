6a4f6d2ae0c853a6f8c34a6f7e674909acb8b8d2
Arjun sn <snarjun92@gmail.com>
2019-07-29 14:16:35 +0530

    migrate out to apollo client

---
+++ src/modules/app/apolloClient.js
@@ -0,0 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.GRAPHQL_ENDPOINT,
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+export default client;
