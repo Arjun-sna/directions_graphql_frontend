45d767446ebd4974c272bf654bc996651167776c
Arjun sn <snarjun92@gmail.com>
2019-08-12 12:17:46 +0530

    included auth token in header of all graphql reqests

--- src/modules/app/apolloClient.js
+++ src/modules/app/apolloClient.js
@@ -3,25 +3,40 @@ import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemo
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import introspectionQueryResultData from './introspection.json';
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData
 });
+const httpLink = new HttpLink({
+  uri: process.env.GRAPHQL_ENDPOINT,
+});
+const errorHandler = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      'x-token': token || '',
+    }
+  }
+});
 const client = new ApolloClient({
   link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: process.env.GRAPHQL_ENDPOINT,
-    })
+    errorHandler,
+    authLink,
+    httpLink,
   ]),
   cache: new InMemoryCache({ fragmentMatcher })
 });
