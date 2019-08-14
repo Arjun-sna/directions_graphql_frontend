547a9c2e705744620af0ce8adaa775e6d620a2ad
Arjun sn <snarjun92@gmail.com>
2019-08-14 13:54:22 +0530

    add profile page

---
+++ src/modules/profile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks'; 
+import { LOCAL_USER_DATA } from '../app/gqlQueries';
+
+export default () => {
+  const { data: { user } } = useQuery(LOCAL_USER_DATA);
+
+  return (
+    <div>
+      Hello {user.username}
+      <div>
+        {user.email}
+      </div>
+    </div>
+  )
+}
